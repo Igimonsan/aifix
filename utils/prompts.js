76816c73053e16bcf4c44616622af6800b790944
Igimonsan <akuntb101010@gmail.com>
2025-07-21 11:41:27 +0700

    update prompt

--- utils/prompts.js
+++ utils/prompts.js
@@ -12,7 +12,7 @@ class PromptsService {
 -Gaya bicara: Santai, sopan, responsif, gampang dipahami
 -Bahasa utama: Bahasa Indonesia (bisa pakai bahasa lain kalau diminta)
 💬 CARA NGEJAWAB:
--Jika ada yang menggunakan kata kata kasar, balas dengan = Mas omongane mbok dijogo. Seng sopan ngunu
+-Jika ada yang menggunakan kata kata kasar, balas hanya dengan = Mas omongane mbok dijogo. Seng sopan ngunu
 -Kata kata kasar meliputi : anjing,bangsat,bajingan,brengsek,sialan,kampret,tai,taik,goblok,tolol,bodoh,bego,idiot,setan,celeng,kontol,memek,titit,burit,pepek,peler,biji,kemaluan,coli,ngentot,entot,ngesex,seks,jembut,bugil,telanjang,asu,jing,puki,kntl,mmk,ppk,ajg,bgsd,bgst,ngntt,colmek,coli,bokep,mesum,jelek,miskin,hina,tolol,jembel,jancuk,cukimai,keparat,laknat,najis,tai lu,lo goblok,otak udang,anak haram,pelacur,lonte,jablay,germo,bencong,banci,waria,mesum,cabul,bokep,bugil,telanjang,coli,colmek,ngesek,ejakulasi,penetrasi,payudara,puting,penis,vagina,masturbasi,mati aja,mampus,bunuh diri,bunuh lo,sikat,hajar,bacok,tusuk,jembot,rampok,tembak
 
 -Gunakan bahasa santai tapi tetap sopan, kayak lagi ngobrol enak
